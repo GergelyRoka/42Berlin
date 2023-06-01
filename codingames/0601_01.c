c919a2e82233f0842157d0979594f1af5b856bfd
GergelyRoka <50467112+GergelyRoka@users.noreply.github.com>
2023-06-01 13:50:47 +0200

    Update 0601_01.c
    make dijkstra readeble

--- codingames/0601_01.c
+++ codingames/0601_01.c
@@ -14,9 +14,9 @@ typedef struct
 	int resources;			// the current amount of eggs/crystals on this cell
 	int my_ants;			// the amount of your ants on this cell
 	int opp_ants;			// the amount of opponent ants on this cell
-	int	ways[MAX];
-	int distances[MAX];
-	int beacon;
+	int	ways[MAX];			//ways from other cells
+	int distances[MAX];		//distances from other cells
+	int beacon;				//1: beacon on on the cell - 0: no beacon
 } cell;
 cell *g_table;
 int g_numberOfCells; // amount of hexagonal cells in this map
@@ -98,40 +98,40 @@ int cost[MAX][MAX],distance[MAX],pred[MAX];
 	//pred[] stores the predecessor of each node
 	//count gives the number of nodes seen so far
 	//create the cost matrix
-	for(i=0;i<n;i++)
-		for(j=0;j<n;j++)
-			if(G[i][j]==0)
-				cost[i][j]=INFINITY;
+	for(i = 0; i < n; i++)
+		for(j = 0; j < n; j++)
+			if(G[i][j] == 0)
+				cost[i][j] = INFINITY;
 			else
-				cost[i][j]=G[i][j];
+				cost[i][j] = G[i][j];
 	//initialize pred[],distance[] and visited[]
-	for(i=0;i<n;i++)
+	for(i = 0; i < n; i++)
 	{
-		distance[i]=cost[startnode][i];
-		pred[i]=startnode;
-		visited[i]=0;
+		distance[i] = cost[startnode][i];
+		pred[i] = startnode;
+		visited[i] = 0;
 	}
-	distance[startnode]=0;
-	visited[startnode]=1;
-	count=1;
-	while(count<n-1)
+	distance[startnode] = 0;
+	visited[startnode] = 1;
+	count = 1;
+	while(count < n - 1)
 	{
-		mindistance=INFINITY;
+		mindistance = INFINITY;
 		//nextnode gives the node at minimum distance
-		for(i=0;i<n;i++)
-			if(distance[i]<mindistance&&!visited[i])
+		for(i = 0; i < n; i++)
+			if(distance[i] < mindistance && !visited[i])
 			{
-				mindistance=distance[i];
-				nextnode=i;
+				mindistance = distance[i];
+				nextnode = i;
 			}
 		//check if a better path exists through nextnode
-		visited[nextnode]=1;
-		for(i=0;i<n;i++)
+		visited[nextnode] = 1;
+		for(i = 0; i < n; i++)
 			if(!visited[i])
-				if(mindistance+cost[nextnode][i]<distance[i])
+				if(mindistance + cost[nextnode][i] < distance[i])
 				{
-					distance[i]=mindistance+cost[nextnode][i];
-					pred[i]=nextnode;
+					distance[i] = mindistance + cost[nextnode][i];
+					pred[i] = nextnode;
 				}
 		count++;
 	}
