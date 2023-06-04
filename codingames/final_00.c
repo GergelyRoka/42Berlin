ebed12a92baa8fbd8e230c844f0bc78e64b7d4f2
GergelyRoka <50467112+GergelyRoka@users.noreply.github.com>
2023-06-05 00:59:23 +0200

    Update final_00.c
    dijsktra love

--- codingames/final_00.c
+++ codingames/final_00.c
@@ -74,6 +74,84 @@ void MY_print_matrix()
 	}
 }
 
+void dijkstra(int startnode)
+{
+	int (*G)[MAX] = g_matrix;
+	int n = g_size;
+	int cost[MAX][MAX], distance[MAX], pred[MAX];
+	int visited[MAX], count, mindistance, nextnode, i, j;
+	//pred[] stores the predecessor of each node
+	//count gives the number of nodes seen so far
+	//create the cost matrix
+	for(i = 0; i < n; i++)
+		for(j = 0; j < n; j++)
+			if(G[i][j] == 0)
+				cost[i][j] = INFINITY;
+			else
+				cost[i][j] = G[i][j];
+	//initialize pred[],distance[] and visited[]
+	for(i = 0; i < n; i++)
+	{
+		distance[i] = cost[startnode][i];
+		pred[i] = startnode;
+		visited[i] = 0;
+	}
+	distance[startnode] = 0;
+	visited[startnode] = 1;
+	count = 1;
+	while(count < n - 1)
+	{
+		mindistance = INFINITY;
+		//nextnode gives the node at minimum distance
+		for(i = 0; i < n; i++)
+			if(distance[i] < mindistance && !visited[i])
+			{
+				mindistance = distance[i];
+				nextnode = i;
+			}
+		//check if a better path exists through nextnode
+		visited[nextnode] = 1;
+		for(i = 0; i < n; i++)
+			if(!visited[i])
+				if(mindistance + cost[nextnode][i] < distance[i])
+				{
+					distance[i] = mindistance + cost[nextnode][i];
+					pred[i] = nextnode;
+				}
+		count++;
+	}	
+	//copy //i wasn't able to handle this with pointers... so :3
+	for (int i = 0; i < n; ++i)
+	{
+		g_table[startnode].distances[i] = distance[i];
+		g_table[startnode].ways[i] = pred[i];
+	}
+	/*
+	//print the path and distance of each node
+	for(i = 0; i < n; i++)
+		if(i != startnode)
+		{
+			fprintf(stderr,"\nDistance of node%d=%d",i, g_table[startnode].distances[i]);
+			fprintf(stderr,"\nPath=%d",i);
+			j=i;
+			do
+			{
+				j=g_table[startnode].ways[j];
+				fprintf(stderr, "<-%d",j);
+			}while(j!=startnode);
+		}
+	*/
+}
+
+void MY_print_distance(int cell)
+{
+	for (int i = 0; i < g_size; ++i)
+	{
+		fprintf(stderr, "%i-%i,",i, g_table[cell].distances[i]);
+	}
+	fprintf(stderr,"\n");
+}
+
 int main()
 {
 	scanf("%d", &g_size); 
@@ -95,6 +173,9 @@ int main()
 
 	MY_init_matrix();
 	MY_print_matrix();
+	dijkstra(info.my_base[0]);
+	dijkstra(info.opp_base[0]);
+	info.game_type == 2 ? dijkstra(info.my_base[1]), dijkstra(info.opp_base[1]) : 0;
 	// game loop
 	while (1)
 	{
