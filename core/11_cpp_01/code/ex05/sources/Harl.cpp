83929679cefe7383bceb9113b9b2d2271fc35706
GergelyRoka <50467112+GergelyRoka@users.noreply.github.com>
2023-11-11 00:37:58 +0100

    fancy messages

--- core/11_cpp_01/code/ex05/sources/Harl.cpp
+++ core/11_cpp_01/code/ex05/sources/Harl.cpp
@@ -6,11 +6,13 @@
 /*   By: groka <groka@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 22:46:22 by groka             #+#    #+#             */
-/*   Updated: 2023/11/10 23:21:13 by groka            ###   ########.fr       */
+/*   Updated: 2023/11/11 00:34:52 by groka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Harl.hpp"
+
+Harl::Harl( void ) {}
 
 void	Harl::complain( std::string level )
 {
@@ -26,20 +28,20 @@ void	Harl::complain( std::string level )
 
 void	Harl::debug( void )
 {
-	std::cout << "DEBUG: I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+	std::cout << DEBUG_MSG << std::endl;
 }
 
 void	Harl::info( void )
 {
-	std::cout << "INFO: I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!" << std::endl;
+	std::cout << INFO_MSG << std::endl;
 }
 
 void	Harl::warning( void )
 {
-	std::cout << "WARNING: I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
+	std::cout << WARNING_MSG << std::endl;
 }
 
 void	Harl::error( void )
 {
-	std::cout << "ERROR: This is unacceptable! I want to speak to the manager now." << std::endl;
+	std::cout << ERROR_MSG << std::endl;
 }
