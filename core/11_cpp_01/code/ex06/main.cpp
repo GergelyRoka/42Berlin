8cad710b34cca9db2cbe78dd6ac060a2c33049c2
GergelyRoka <50467112+GergelyRoka@users.noreply.github.com>
2023-11-11 00:44:11 +0100

    deleting lame lines

--- core/11_cpp_01/code/ex06/main.cpp
+++ core/11_cpp_01/code/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: groka <groka@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 22:59:54 by groka             #+#    #+#             */
-/*   Updated: 2023/11/11 00:27:29 by groka            ###   ########.fr       */
+/*   Updated: 2023/11/11 00:43:08 by groka            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,8 @@ static int	param_check(int argc, char const *argv[]);
 int main(int argc, char const *argv[])
 {
 	int	param = param_check(argc, argv);
-
-	if (param == -1)
-	{
-		std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
-		return (0);
-	}
 	Harl harl;
+	
 	switch (param)
 	{
 		case (0):
@@ -39,7 +34,7 @@ int main(int argc, char const *argv[])
 			harl.complain("ERROR");
 			break;
 		default:
-			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+			std::cout << "\033[36m[ Probably complaining about insignificant problems ]" << std::endl;
 	}
 	return 0;
 }
@@ -48,8 +43,6 @@ static int	param_check(int argc, char const *argv[])
 {
 	if (argc == 1)
 		return (0);
-	
-	enum level_enum {INVALID = -1, DEBUG, INFO, WARNING, ERROR};
 	std::string param = argv[1];
 	if (param == "DEBUG")
 		return (0);
