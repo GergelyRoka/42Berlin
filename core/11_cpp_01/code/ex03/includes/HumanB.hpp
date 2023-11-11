d32017eacc3f9fd7a6e99129660f468050890465
GergelyRoka <50467112+GergelyRoka@users.noreply.github.com>
2023-11-11 01:00:47 +0100

    header guard

--- core/11_cpp_01/code/ex03/includes/HumanB.hpp
+++ core/11_cpp_01/code/ex03/includes/HumanB.hpp
@@ -10,6 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
 #include <string>
 #include <iostream>
 #include "Weapon.hpp"
